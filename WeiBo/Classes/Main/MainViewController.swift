dd96dca123d23235388769d1390341fc765f479a
zhangshuqing <zhangshuqing@zhangshingdeMBP.lan>
2016-05-22 21:33:31 +0800

    添加加号

--- WeiBo/Classes/Main/MainViewController.swift
+++ WeiBo/Classes/Main/MainViewController.swift
@@ -9,13 +9,75 @@
 import UIKit
 
 class MainViewController: UITabBarController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         //设置当前控制器对应的tabbar的颜色
         //注意: 如果iOS7以前如果设置了tintcolor只有文字会变, 而图片不会变
         tabBar.tintColor = UIColor.orangeColor()
         
+        addChildViewControllers()
+    }
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        //添加加号按钮
+        setupComposeBtn()
+    }
+    
+    //监听按钮的点击
+    //注意: 监听按钮点击的方法不能是私有方法
+    //按钮点击事件的调用是由 运行循环 监听并以消息机制传递的, 因此, 监听函数不能设置为private
+    func composeBtnClick() {
+        print(__FUNCTION__)
+    }
+    
+    private func setupComposeBtn() {
+        //1.添加加号按钮
+        tabBar.addSubview(composeBtn)
+        
+        //2.调整加号的位置
+        let width = UIScreen.mainScreen().bounds.size.width / CGFloat( viewControllers!.count)
+        let rect = CGRect(x: 0, y: 0, width: width, height: 49)
+        composeBtn.frame = rect
+        composeBtn.frame = CGRectOffset(rect, 2 * width, 0)
+    }
+    
+    //初始化子控制器
+    private func addChildViewController(childControllerName: String, title:String, imageName:String) {
+        
+        //1.动态获取命名空间
+        let ns = NSBundle.mainBundle().infoDictionary!["CFBundleExecutable"] as! String
+        
+        //0.将字符串转换为类
+        //0.1命名空间默认情况下就是项目名称 , 但是命名空间名称是可以修改的
+        let cls:AnyClass? = NSClassFromString(ns + "." + childControllerName)
+        //0.2通过类创建对象
+        //0.2.1将AnyClass转换为指定的类型
+        let vcCls = cls as! UIViewController.Type
+        //0.2.2通过class创建对象
+        let vc = vcCls.init()
+        
+        
+        
+        //1.1设置首页tabbar对应的数据
+        vc.tabBarItem.image = UIImage(named: imageName)
+        vc.tabBarItem.selectedImage = UIImage(named: imageName + "_highlighted")
+        vc.tabBarItem.title = title
+        
+        //1.2设置导航条对应的数据
+        //home.navigationItem.title = "首页"
+        vc.title = title
+        
+        //2.给首页包装一个导航控制器
+        let nav = UINavigationController()
+        nav.addChildViewController(vc)
+        
+        addChildViewController(nav)
+    }
+    
+    //添加所有自控制器
+    private func addChildViewControllers() {
         //1.获取json文件路径
         let path = NSBundle.mainBundle().pathForResource("MainVCSettings.json", ofType: nil)
         //2.通过文件路径创建NSData
@@ -45,40 +107,21 @@ class MainViewController: UITabBarController {
             }
             
         }
+        
+        
     }
     
-    //初始化子控制器
-    private func addChildViewController(childControllerName: String, title:String, imageName:String) {
+    //MARk 懒加载
+    lazy var composeBtn:UIButton = {
+        let btn = UIButton()
+        btn.setImage(UIImage(named: "tabbar_compose_icon_add"), forState: UIControlState.Normal)
+        btn.setImage(UIImage(named: "tabbar_compose_icon_add_highlighted"), forState: UIControlState.Highlighted)
+        btn.setBackgroundImage(UIImage(named: "tabbar_compose_button"), forState: UIControlState.Normal)
+        btn.setBackgroundImage(UIImage(named: "tabbar_compose_button_highlighted"), forState: UIControlState.Highlighted)
         
-        //1.动态获取命名空间
-        let ns = NSBundle.mainBundle().infoDictionary!["CFBundleExecutable"] as! String
-
+        //添加监听
+        btn.addTarget(self, action: "composeBtnClick", forControlEvents: UIControlEvents.TouchUpInside)
         
-        //0.将字符串转换为类
-        //0.1命名空间默认情况下就是项目名称 , 但是命名空间名称是可以修改的
-        let cls:AnyClass? = NSClassFromString(ns + "." + childControllerName)
-        //0.2通过类创建对象
-        //0.2.1将AnyClass转换为指定的类型
-        let vcCls = cls as! UIViewController.Type
-        //0.2.2通过class创建对象
-        let vc = vcCls.init()
-        
-        
-        
-        //1.1设置首页tabbar对应的数据
-        vc.tabBarItem.image = UIImage(named: imageName)
-        vc.tabBarItem.selectedImage = UIImage(named: imageName + "_highlighted")
-        vc.tabBarItem.title = title
-        
-        //1.2设置导航条对应的数据
-        //home.navigationItem.title = "首页"
-        vc.title = title
-        
-        //2.给首页包装一个导航控制器
-        let nav = UINavigationController()
-        nav.addChildViewController(vc)
-    
-        addChildViewController(nav)
-    }
-   
+        return btn
+    }()
 }
