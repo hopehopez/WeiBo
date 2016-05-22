38821f745fc0184fd39bd2d612cfcb6ea9e7ccfe
zhangshuqing <zhangshuqing@zhangshingdeMBP.lan>
2016-05-22 18:08:11 +0800

    搭建基本框架

--- WeiBo/Classes/Main/MainViewController.swift
+++ WeiBo/Classes/Main/MainViewController.swift
@@ -12,24 +12,36 @@ class MainViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        //设置当前控制器对应的tabbar的颜色
+        //注意: 如果iOS7以前如果设置了tintcolor只有文字会变, 而图片不会变
+        tabBar.tintColor = UIColor.orangeColor()
+        
+        
+        addChildViewController(HomeTableViewController(),title: "首页", imageName: "tabbar_home")
+        addChildViewController(MessageTableViewController(), title: "消息", imageName:  "tabbar_message_center")
+        addChildViewController(DiscoverTableViewController(), title: "发现", imageName: "tabbar_discover")
+        addChildViewController(ProfileTableViewController(), title: "我的", imageName: "tabbar_profile")
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    //初始化子控制器
+    private func addChildViewController(childController: UIViewController, title:String, imageName:String) {
+        
+        //1.1设置首页tabbar对应的数据
+        childController.tabBarItem.image = UIImage(named: imageName)
+        childController.tabBarItem.selectedImage = UIImage(named: imageName + "_highlighted")
+        childController.tabBarItem.title = title
+        
+        //1.2设置导航条对应的数据
+        //home.navigationItem.title = "首页"
+        childController.title = title
+        
+        //2.给首页包装一个导航控制器
+        let nav = UINavigationController()
+        nav.addChildViewController(childController)
+        
+        //3.
+        addChildViewController(nav)
     }
-    */
-
+   
 }
