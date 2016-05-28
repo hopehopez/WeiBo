35b0b2f2755d44d4877d1ae15d0198b7d58fee7c
zhangshuqing <zhangshuqing@zhangshingdeMBP.lan>
2016-05-28 20:40:24 +0800

    自定义转场

--- WeiBo/Classes/Home/HomeTableViewController.swift
+++ WeiBo/Classes/Home/HomeTableViewController.swift
@@ -29,7 +29,7 @@ class HomeTableViewController: BaseTableViewController {
         
         //2.初始化标题按钮
         let titleBtn = TitleButton()
-        titleBtn.setTitle("即可江南", forState: UIControlState.Normal)
+        titleBtn.setTitle("极客江南 ", forState: UIControlState.Normal)
         
         titleBtn.addTarget(self, action: "titleBtnClick:", forControlEvents: UIControlEvents.TouchUpInside)
         
@@ -43,7 +43,28 @@ class HomeTableViewController: BaseTableViewController {
         print("点了右边的")
     }
     func titleBtnClick(btn: UIButton) {
+        //1.修改箭头方向
         btn.selected = !btn.selected;
+        
+        //2.弹出菜单
+        let sd = UIStoryboard(name: "PopoverViewController", bundle: nil)
+        let vc = sd.instantiateInitialViewController()
+        //2.1设置转场代理
+        //默认情况下modal 会移除以前控制器的view, 替换为当前弹出的view
+        //如果自定义转场, 那么就不会移除以前控制器的view
+        vc?.transitioningDelegate = self
+        //2.2设置转场的样式
+        vc?.modalPresentationStyle = UIModalPresentationStyle.Custom
+        presentViewController(vc!, animated: true, completion: nil)
+        
     }
    
+}
+
+extension HomeTableViewController: UIViewControllerTransitioningDelegate{
+    //实现代理方法, 告诉系统谁来负责转场动画
+    //UIPresentationController iOS8推出的专门负责转场动画的
+    func presentationControllerForPresentedViewController(presented: UIViewController, presentingViewController presenting: UIViewController, sourceViewController source: UIViewController) -> UIPresentationController? {
+        return PopoverPresentationController(presentedViewController: presented, presentingViewController: presenting)
+    }
 }
---
+++ WeiBo/Classes/Home/Popover/PopoverPresentationController.swift
@@ -0,0 +1,51 @@
+//
+//  PopoverPresentationController.swift
+//  WeiBo
+//
+//  Created by zsq on 16/5/28.
+//  Copyright © 2016年 zsq. All rights reserved.
+//
+
+import UIKit
+
+class PopoverPresentationController: UIPresentationController {
+    //初始化方法, 用于创建负责转场动画的对象
+    //参数一: 被展现的控制器
+    //参数二: 发起的控制器, Xcode6是nil , XCode7是野指针
+    //return: 负责转场动画的对象
+    override init(presentedViewController: UIViewController, presentingViewController: UIViewController) {
+        
+        super.init(presentedViewController: presentedViewController, presentingViewController: presentingViewController)
+        
+        print(presentedViewController)
+        //print(presentingViewController)
+    }
+    
+    //即将布局转场子视图时调用
+    override func containerViewWillLayoutSubviews() {
+        //1.修改弹出视图的大小
+        //containerView 容器视图
+        //presentView() 被展现的view
+        presentedView()?.frame = CGRectMake(100, 56, 200, 200)
+        //2.在容器视图上添加蒙版, 插入到展现视图的下面
+        //因为展现视图和蒙版都在一个视图上, 而后添加的会盖住先添加的
+        containerView?.insertSubview(cover, atIndex: 0)
+    }
+    //MARK - 懒加载
+    private lazy var cover: UIView = {
+        //创建view
+        let view = UIView()
+        view.backgroundColor = UIColor(white: 0.0, alpha: 0.2)
+        view.frame = UIScreen.mainScreen().bounds
+        
+        //添加手势
+        let tap = UITapGestureRecognizer(target: self, action: "close")
+        view.addGestureRecognizer(tap)
+        
+        return view
+    }()
+    
+    func close() {
+        presentedViewController.dismissViewControllerAnimated(true, completion: nil)
+    }
+}
